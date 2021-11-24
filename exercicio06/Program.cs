760475cb927496c86bedc8d6136289a76d922322
eduardopaimsilva <edups.ww.com@hotmail.com>
2021-11-24 15:44:55 -0300

    exercici06

---
+++ exercicio06/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercicio06
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          Console.WriteLine("Digiti um numero para dia da semana(1 a 7)");
+          int diaDaSemana = Convert.ToInt32(Console.ReadLine());
+
+          switch (diaDaSemana)
+          {
+            case 1:
+            Console.WriteLine("domingo");
+            break;
+            case 2:
+            Console.WriteLine("segunda");
+            break;
+            case 3:
+            Console.WriteLine("terça");
+            break;
+            case 4:
+            Console.WriteLine("quarta feira");
+            break;
+            case 5:
+            Console.WriteLine("quinta feira");
+            break;
+            case 6:
+            Console.WriteLine("fexta feira");
+            break;
+            case 7:
+            Console.WriteLine("fexta feira");
+            break;
+            default:
+            Console.WriteLine("numero de 1 a 7");
+              break;
+          }
+          
+
+        }
+    }
+}
